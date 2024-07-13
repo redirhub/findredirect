1ee3e4cb17578d0684a02c0ee1592d628dfc0de6
krisbob <24961323+krisboblea@users.noreply.github.com>
2024-07-13 12:28:33 +1000

    fix Cache-Control for vercel edge cache

--- src/pages/api/data.js
+++ src/pages/api/data.js
@@ -1,14 +1,29 @@
 import { fetchData } from "./fetch";
 
+let cachedData = null;
+let cacheTimestamp = 0;
+
 export default async function handler(req, res) {
   try {
-    const data = await fetchData();
+    // Check if cached data exists and is not expired
 
-    res.setHeader("Cache-Control", `max-age=${process.env.CACHE_EXPIRES_AFTER_SECONDS || 3600}`);
+    // Determine appropriate message based on whether data was fetched from cache or not
+    let msg = "Data fetched from memory cache";
 
-    return res.json({ data, msg: "Data fetched from API!" });
+    if (!cachedData || Date.now() > cacheTimestamp + parseInt(process.env.CACHE_EXPIRES_AFTER_SECONDS || 3600) * 1000) {
+      // Fetch fresh data if cache is expired or doesn't exist
+      cachedData = await fetchData();
+      cacheTimestamp = Date.now();
+      msg = "Data fetched from API";
+    }
+
+    // Set Cache-Control header with s-maxage based on environment variable
+    res.setHeader("Cache-Control", `public, s-maxage=${process.env.CACHE_EXPIRES_AFTER_SECONDS || 3600}`);
+
+    // Respond with cached data
+    return res.json({ data: cachedData, msg });
   } catch (error) {
-    console.error(error);
+    console.error("Request error:", error);
     return res.status(500).json({ error: error.message });
   }
 }
--- src/pages/api/fetch.js
+++ src/pages/api/fetch.js
@@ -1,15 +1,11 @@
 import axios from "axios";
 
-let cachedData = null;
-let cacheTimestamp = 0;
-
 // Set axios instance defaults
 axios.defaults.params = {
   [ "api-key" ]: process.env.API_KEY,
 };
 axios.defaults.baseURL = process.env.API_BASE;
 
-// Function to fetch data from external API
 export async function fetchData() {
   try {
     const { SITES } = process.env;
@@ -35,34 +31,9 @@ export async function fetchData() {
       timestamp: Date.now(),
     };
 
-    // Update cached data and timestamp
-    cachedData = data;
-    cacheTimestamp = Date.now();
-
     return data;
   } catch (error) {
     console.error("Error fetching data:", error);
     throw error; // Throw error to be handled by caller
-  }
-}
-
-// Request handler function
-export default async function handler(req, res) {
-  try {
-    // Check if cached data exists and is not expired
-    if (!cachedData || Date.now() > cacheTimestamp + parseInt(process.env.CACHE_EXPIRES_AFTER_SECONDS || 3600) * 1000) {
-      // Fetch fresh data if cache is expired or doesn't exist
-      cachedData = await fetchData();
-      cacheTimestamp = Date.now();
-    }
-
-    // Set Cache-Control header with max-age based on environment variable
-    res.setHeader("Cache-Control", `max-age=${process.env.CACHE_EXPIRES_AFTER_SECONDS || 3600}`);
-
-    // Respond with cached data
-    return res.json({ data: cachedData, msg: "Data fetched from cache!" });
-  } catch (error) {
-    console.error("Request error:", error);
-    return res.status(500).json({ error: error.message });
   }
 }
