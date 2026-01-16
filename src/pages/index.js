2f592b4c5bebea78f399b723f250f613fc279cb1
Krisbo <24961323+krisboblea@users.noreply.github.com>
2026-01-16 23:40:06 +0200

    dev mode debug

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,24 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { fetchAllPagesForFooter, fetchPageBySlug } from "@/services/pageService";
 import ToolPage from "./[tool]";
+import MainLayout from '@/layouts/MainLayout';
+import { AppContainer } from '@/components/common/AppContainer';
+import { Box, Heading, VStack } from '@chakra-ui/react';
 
 const Home = ({ homeData, pages = [] }) => {
   // If homepage exists in CMS with slug "home", use it
+  const isDev = process.env.NODE_ENV === 'development';
+
+  return (
+    <MainLayout pages={pages}>
+      <AppContainer>
+        <Box textAlign="center" py={20}>
+          <Heading size="2xl">Development Mode</Heading>
+        </Box>
+      </AppContainer>
+    </MainLayout>
+  );
+
   if (homeData) {
     return <ToolPage toolData={homeData} pages={pages} />;
   }
