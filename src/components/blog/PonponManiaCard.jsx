59918a81c62865ae2012b8d44fae3fc15b10b0f3
Krisbo <24961323+krisboblea@users.noreply.github.com>
2026-01-16 18:51:19 +0200

    - Remove the H1 tag from the featured (pinned) article title on the blog page.

--- src/components/blog/PonponManiaCard.jsx
+++ src/components/blog/PonponManiaCard.jsx
@@ -43,7 +43,7 @@ export default function PonponManiaCard({ post, reverse = false }) {
         >
           <Box flex="1">
             <Heading
-              as="h1"
+              as="span"
               fontSize={{ base: "24px", md: "28px", lg: "32px" }}
               fontWeight="700"
               lineHeight="1.3"
