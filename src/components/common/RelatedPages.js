1d6bf3f59373645a9ec83ab207f448be6f34bb2e
Krisbo <24961323+krisboblea@users.noreply.github.com>
2026-02-15 21:06:27 +0400

    i18n

--- src/components/common/RelatedPages.js
+++ src/components/common/RelatedPages.js
@@ -1,18 +1,20 @@
 import { Box, Heading, List, ListItem, Link } from '@chakra-ui/react';
 import NextLink from 'next/link';
+import { useTranslation } from 'next-i18next';
 
 export default function RelatedPages({ pages = [] }) {
+    const { t } = useTranslation();
     if (!pages || pages.length === 0) return null;
 
     return (
         <Box mt={12} mb={12}>
             <Heading as="p" size="lg" mb={4}>
-                Related Pages
+                {t('tool.related-pages', 'Related Pages')}
             </Heading>
             <List spacing={2}>
                 {pages.map((page) => (
                     <ListItem key={page._id}>
-                        <NextLink href={`/${page.slug.current}`} passHref>
+                        <NextLink href={`/${page.slug}`} passHref>
                             <Link color="purple.600" _hover={{ color: 'purple.800', textDecoration: 'underline' }}>
                                 {page.title}
                             </Link>
